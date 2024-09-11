ddd70dd9f609eba9e5a06c490b764d5f0575d6c5
Dave Williams <davejjwilliams@gmail.com>
2024-09-11 16:02:56 +0100

    Database Connection Setup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const sweData = require('./dataset-verified.json');
 const explData = require('./patch-explanations.json');
 
 const app = express();
+
+// Connect DB
+connectDB();
 
 app.post('/api/patch', (req, res) => {
   res.json({
