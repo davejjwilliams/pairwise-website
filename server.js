2bbbdb8f7405c22efe2575c7d867dfc0e9d77ed4
Dave Williams <davejjwilliams@gmail.com>
2024-09-11 19:01:15 +0100

    Prepare for Deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const { check, validationResult } = require('express-validator');
+const path = require('path');
 
 const sweData = require('./dataset-verified.json');
 const explData = require('./patch-explanations.json');
@@ -102,6 +103,16 @@ app.post(
     }
   }
 );
+
+// Server Static Assets in Production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/dist'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
